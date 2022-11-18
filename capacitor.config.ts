e89f7a45de5c1a36c23269f131e5483cbaf4db8a
paleru <egil123EMP!>
2022-11-18 13:57:53 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TDS200_1015',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
